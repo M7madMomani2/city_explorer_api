88681f21e60ec122e33db5e00a9d41548adaec4b
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-03-29 01:20:26 +0300

    create js file

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 'use strict';
-
+let weatherArray=[];
 const PORT  = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-
-
 app.get('/location', getLocation);
+app.get('/weather',getWeather);
+
+app.listen(process.env.PORT || PORT, ()=>{
+  console.log(`app is listening on port ${PORT}`);
+});
+
+function Location(search_query, formatted_query, latitude, longitude){
+  this.search_query= search_query;
+  this. formatted_query= formatted_query;
+  this.latitude= latitude;
+  this.longitude = longitude;
+}
+
+function Weather(forecast,time){
+  this.forecast=forecast;
+  this.time = time;
+  weatherArray.push(this);
+
+}
 
 function getLocation(request, response){
   try{
@@ -17,60 +34,29 @@ function getLocation(request, response){
     let locationObject = new Location(city,getLocationObject.display_name,getLocationObject.lat,getLocationObject.lon);
 
     response.send(locationObject);
-  } catch(error){
+  }catch(error){
     response.status(500).send(error)
   }
 
 }
 
-
-
-
-function Location(search_query, formatted_query, latitude, longitude){
-  this.search_query= search_query;
-  this. formatted_query= formatted_query;
-  this.latitude= latitude;
-  this.longitude = longitude;
-}
-
-//   start the weather server
-
-
-app.get('/weather',getWeather);
-
-let weatherArray=[];
-
-function WeatherCnstructor(forecast,time){
-  this.forecast=forecast;
-  this.time = time;
-  weatherArray.push(this);
-
-}
-
 function getWeather(req,res){
   try{
-
     if(weatherArray){
       weatherArray=[];
     }
-
-
     let weatherData = require('./Data/weather.json');
     let weather = weatherData.data;
     weather.forEach(element =>{
-      let newData = new WeatherCnstructor(element.valid_date,element.weather.description);
-
+      let Data = new Weather(element.valid_date,element.weather.description);
+      console.log(Data);
     });
     res.send(weatherArray);
   }
   catch(error){
     res.status(500).send(error)
-
   }
 
 }
 
 
-app.listen(PORT, ()=>{
-  console.log(`app is listening on port ${PORT}`);
-});
